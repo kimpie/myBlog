b7738a65003980c4ba91e706bb8c97a4b9679e40
kimpie <kapienta@gmail.com>
2014-09-09 20:12:34 +1000

    Attaching db

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express'),
         app = express(),
         fs = require('fs'),
         http = require('http'),
-        server = http.createServer(app);
+        server = http.createServer(app),
+        mongodb = require('mongodb');
+
+var uri = 'mongodb://heroku_app29379056:np9fo6f71flbcv3ieeqdp34hvk@ds035270.mongolab.com:35270/heroku_app29379056';
 
 app.configure(function(){
         app.set('view engine', 'handlebars');
@@ -23,6 +26,15 @@ app.get('/', function (req, res){
 app.get('/*', function  (req, res) {
   res.json(404, {status: 'not found'});
 });
+
+//Connect the database
+
+mongodb.MongoClient.connect(uri, function(err, db) {
+  
+  if(err) throw err;
+  var posts = db.collection('posts');
+});
+
 
 server.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
